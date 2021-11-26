27ced47180bc769b71a7f83e01b8d3138279da4e
TimeSea05 <anwentao1@gmail.com>
2021-11-26 20:29:18 +0800

    精简了注释

--- include/game_func.h
+++ include/game_func.h
@@ -1,21 +1,28 @@
 #ifndef GAME_FUNCTIONS_H_
 #define GAME_FUNCTIONS_H_
 
-// @brief 初始化棋盘，将数组record_board的元素全部置为0
+// 初始化棋盘，将数组record_board的元素全部置为0
 void init_record_board();
 
-// @brief 根据数组record_board的值对display_board进行赋值
+// 根据数组record_board的值对display_board进行赋值
 void record_to_display_array();
 
-// @brief 打印字符数组display_board
+// 打印字符数组display_board
 void display_board();
 
-// drop pieces
-// type: 0 for black pieces, 1 for white pieces 
+// 落子
+// @param: type BLACKPIECE: 黑子  WHITEPIECE 白子
 void drop_pieces(int type);
 
-// @brief 判断是否有一方已经胜出
+// 判断是否有一方已经胜出
 int game_win();
 
-int is_forbidden(int x, int y, int direction);
+// 检查禁手
+int is_forbidden();
+
+// 人人对战模式
+void person_vs_person();
+
+// 人机对战模式
+void person_vs_computer();
 #endif
